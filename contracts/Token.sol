807f7ef43643139fe7adeccef9d8a5f41259c8f4
Danil Dovgal <91081974+qweblessed@users.noreply.github.com>
2022-06-20 09:06:18 +0300

    first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OnlyOneAirdrop is ERC20 {
+    constructor() ERC20("OnlyOne", "Only") {
+        _mint(msg.sender, 10 * 10 ** decimals());
+    }
+}
