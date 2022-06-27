f1d283b1d28ba113156ef35f7eb94f3a765644d5
Danil Dovgal <91081974+qweblessed@users.noreply.github.com>
2022-06-27 09:06:46 +0300

    signature

---
+++ contracts/VestingWithSignature.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "hardhat/console.sol";
+
+contract SignatureVesting is Ownable {
+    using ECDSA for bytes32;
+
+    mapping (bytes => bool) signatureClaim;
+    IERC20 immutable _token;
+
+    event Claim(address indexed claimer, uint256 amount);
+
+    constructor(IERC20 vestedToken) {
+        _token = vestedToken;
+    }
+
+    function claimTokens(uint256 amount, uint256 nonce, bytes calldata signature) external {
+        require(canClaimTokens(amount, nonce, signature), "cannot claim");
+
+        signatureClaim[signature] = true;
+        _token.transfer(msg.sender, amount);
+
+        emit Claim(msg.sender, amount);
+    }
+
+    function canClaimTokens(uint256 amount, uint256 nonce, bytes calldata signature) private view returns (bool) {
+        bytes32 message = keccak256(abi.encodePacked(msg.sender, amount, nonce, address(this)));
+
+        return signatureClaim[signature] == false 
+            && message.toEthSignedMessageHash().recover(signature) == owner();
+    }
+}
--- test/index.ts
+++ test/index.ts
@@ -1,66 +1,35 @@
 import { expect } from "chai";
-import { randomBytes, sign } from "crypto";
-import { Wallet } from "ethers";
-import { parseEther } from "ethers/lib/utils";
+import { BigNumber, Signer } from "ethers";
 import { ethers } from "hardhat";
-import keccak256 from "keccak256";
-import MerkleTree from "merkletreejs";
-import { MerkleAirdrop__factory,OnlyOneAirdrop__factory } from '../typechain'
+import { OnlyOneAirdrop__factory, SignatureVesting, SignatureVesting__factory, ERC20 } from "../typechain";
+import keccak256 from 'keccak256';
 
-describe("Vesting", function () {
-    it('Full Cycle',async () => {
-        const [signer,guy] = await ethers.getSigners()
+describe("SignatureVesting", function () {
+  let erc20 : ERC20;
+  let vesting : SignatureVesting;
+  let defaultVestingBalance : BigNumber;
+  
+  beforeEach(async () => {
+    let [signer] = await ethers.getSigners();
+    erc20 = await new OnlyOneAirdrop__factory(signer).deploy();
+    vesting = await new SignatureVesting__factory(signer).deploy(erc20.address);
+    defaultVestingBalance = (await erc20.balanceOf(await signer.getAddress())).div(2);
 
-        const token = await new OnlyOneAirdrop__factory(signer).deploy()
+    await erc20.transfer(vesting.address, defaultVestingBalance);
+  })
+  
+  it("Should emit Vest event with correct data when vesting tokens", async function () {
+    let address2 = await ethers.provider.getSigner(1).getAddress();
+    let address3 = await ethers.provider.getSigner(2).getAddress();
+    console.log('div',defaultVestingBalance.div(2));
+    console.log('addre',address3)
+    let message2 = ethers.utils.solidityPack(["address", "uint256", "uint256", "address"], ['0xdA0Fb2305EEad572fEAe4ee8C058C75760D49154', '2500000000000000000', 0,'0xb2D6cBAce476C59F90E86d75a010686373aB5C8C']);
+    let message3 = ethers.utils.solidityPack(["address", "uint256", "uint256", "address"], [address3, defaultVestingBalance.div(3), 0, vesting.address]);
+    
+    let signature2 = await (await ethers.provider.getSigner(0)).signMessage(keccak256(message2)); // owner signs the message
+    let signature3 = await (await ethers.provider.getSigner(0)).signMessage(keccak256(message3));
+    console.log('message2',signature2)
+    console.log('msg3',message3)
 
-        const randomAddresses = new Array(15)
-        .fill(0)
-        .map(()=> new Wallet(randomBytes(32).toString('hex')).address)
-
-        const merkleTree = new MerkleTree(
-            randomAddresses.concat(signer.address),
-            keccak256,
-            {hashLeaves:true,sortPairs:true}
-        )
-
-        const root = merkleTree.getHexRoot()
-
-        const airdrop = await new MerkleAirdrop__factory(signer).deploy(
-            token.address,
-            root 
-        )
-        
-        await token.transfer(airdrop.address,parseEther('10'))
-        
-        const proof = merkleTree.getHexProof(keccak256(signer.address))
-
-        expect(await airdrop.claimed(signer.address)).to.eq(false)
-
-        expect(await airdrop.canClaim(signer.address,proof)).to.eq(true)
-
-        await expect(() => airdrop.claim(proof)).to.changeTokenBalances(
-          token,
-          [airdrop,signer],
-          [parseEther('-1'),parseEther('1')]
-        )
-
-        expect(await airdrop.claimed(signer.address)).to.eq(true)
-
-        expect(await airdrop.canClaim(signer.address,proof)).to.eq(false)
-
-        await expect(airdrop.claim(proof)).to.be.revertedWith(
-          'MerkleAirdrop: Address is not a candidate for claim'
-        )
-
-        const badProof = merkleTree.getHexProof(keccak256(guy.address))
-
-        expect(badProof).to.eql([])
-
-        expect(await airdrop.canClaim(guy.address,badProof)).to.eq(false)
-
-        await expect(airdrop.connect(guy).claim(badProof)).to.be.revertedWith(
-          'MerkleAirdrop: Address is not a candidate for claim'
-        )
-
-    })
+  });
 });
